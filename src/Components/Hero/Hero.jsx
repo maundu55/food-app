6bb29e7b016d5465a1101800acd2444afa1eba25
maundu55 <marymaundu.mm@gmail.com>
2025-09-25 10:30:03 +0300

    add hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import hero_img from '../../assets/pasta2.png'
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+      <div className='hero-container'>
+        <div className='hero_content'>
+          <h1>GEN-Z DINER</h1>
+          <img src={hero_img} alt="" className='hero_img'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../../Components/Hero/Hero'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Hero />
+    </div>
   )
 }
 
