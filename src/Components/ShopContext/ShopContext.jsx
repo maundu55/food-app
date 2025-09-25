32dd414e05eabd77d7c3d55c53d7c459925f66c9
maundu55 <marymaundu.mm@gmail.com>
2025-09-25 15:34:24 +0300

    calculate cart total

--- src/Components/ShopContext/ShopContext.jsx
+++ src/Components/ShopContext/ShopContext.jsx
@@ -7,6 +7,20 @@ export const ShopContext = createContext();
 const ShopContextProvider =({children}) =>{
 
         const [products, setProducts] = useState(productsData);
+        const [cart, setCart] = useState([]);
+        const [quantity, setQuantity] = useState(0);
+        const [total, setTotal] = useState(0);
+
+        useEffect(()=>{
+            const total = cart.reduce((accumulator,currentItem)=>{
+                const priceAsNumber = parseFloat(currentItem.price);
+                if(isNaN(priceAsNumber)){
+                    return accumulator;
+                }
+                return accumulator + priceAsNumber * currentItem.amount;
+            }, 0)
+            setTotal(total);
+        }, [cart])
 
     return(
         <ShopContext.Provider value ={{products}}>
