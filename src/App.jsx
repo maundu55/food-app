c1ae8233005f1c7e03bedeb234037e46d20e96a5
maundu55 <marymaundu.mm@gmail.com>
2025-09-25 10:16:28 +0300

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from './Components/NavBar/NavBar'
+import Homepage from './Pages/Homepage/Homepage'
+import Footer from './Components/Footer/Footer'
+import Cart from './Components/Cart/Cart'
+import ProductDetails from './Pages/ProductDetails/ProductDetails'
 import {Routes , Route} from 'react-router-dom'
 
 const App = () => {
@@ -7,10 +11,11 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<h1>Home Page</h1>} />
-
-        
+        <Route path='/' element={<Homepage />} />
+      <Route path='/cart' element ={<Cart />} />
+      <Route path ='/product/:id' element ={<ProductDetails />} />
         </Routes>
+        <Footer />
     </div>
   )
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BiCart } from 'react-icons/bi'
+import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <div className='navbar'>
+      <div className='link'>
+       <ul>
+        <li>HOME</li>
+       <li>DISHES</li>
+       </ul>
+      </div>
+      <div className='logo-header'>
+        <h2>GEN-Z DINER</h2>
+      </div>
+      <div className='nav_icon_wrapper'>
+        <BiCart className='nav-icon' />
+        <p className='nav_qty'>0</p>
+      </div>
+      </div>
+    </div>
   )
 }
 
